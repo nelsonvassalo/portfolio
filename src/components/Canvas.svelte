44102d180d58f1a254ed4930c9dc2cf372139e2a
Nelson Vassalo <vassalo.nelson@gmail.com>
2023-10-03 19:30:43 +0100

    Stable Three js refactor

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,21 +1,29 @@
 <script>
-	import {
-		Renderer,
-		TextureLoader,
-		Mesh,
-		Transform,
-		Program,
-		Box,
-		Geometry,
-		Vec2,
-		Plane,
-		Camera
-	} from 'ogl';
-	import fragment from '../code/js/shaders/frag.glsl';
-	import vertex from '../code/js/shaders/vertex.glsl';
 	import { onMount } from 'svelte';
 
+	import {
+		WebGLRenderer,
+		Mesh,
+		BufferGeometry,
+		Vector2,
+		Scene,
+		PerspectiveCamera,
+		MeshBasicMaterial,
+		PlaneGeometry,
+		DoubleSide,
+		VideoTexture,
+		LinearFilter,
+		RGB_ETC1_Format,
+		RGBAIntegerFormat,
+		sRGBEncoding
+	} from 'three';
+
+	import fragment from '../code/js/shaders/frag.glsl';
+	import vertex from '../code/js/shaders/vertex.glsl';
+	import RoundedRect from '../code/js/RoundedRectangle';
+
 	import { scroll, glScale, glY } from '../code/js/store';
+	import RoundedRectangle from '../code/js/RoundedRectangle';
 	let canvas;
 
 	$: console.log($scroll);
@@ -23,97 +31,85 @@
 	onMount(() => {
 		const wW = window.innerWidth;
 		const wH = window.innerHeight;
-		const renderer = new Renderer({
-			width: wW,
-			height: wH
+		const renderer = new WebGLRenderer({
+			antialias: true
 		});
-		const gl = renderer.gl;
-		canvas.appendChild(gl.canvas);
+		console.log('🚀 ~ renderer:', renderer);
+		renderer.setSize(wW, wH);
 
-		gl.clearColor(1, 1, 1, 0);
+		let vec = new Vector2();
 
-		const scene = new Transform();
-		const fov = 45;
+		canvas.appendChild(renderer.domElement);
 
-		const camera = new Camera(gl, { fov, far: 5000 });
-		camera.perspective({ aspect: gl.canvas.width / gl.canvas.height });
+		renderer.setClearColor(0xfff, 0);
 
-		const z = (gl.canvas.height / Math.tan((fov * Math.PI) / 360)) * 0.5;
+		const scene = new Scene();
+		const fov = 55;
 
+		const camera = new PerspectiveCamera(fov, wW / wH, 0.01, 5000);
+
+		const z = (wW / Math.tan((fov * Math.PI) / 360)) * 0.5;
 		camera.position.set(0, 0, z);
 		// camera.lookAt([0, 0, 0]);
 
 		function resize() {
 			renderer.setSize(wW, wH);
-			camera.perspective({ aspect: gl.canvas.width / gl.canvas.height });
+			// camera.perspective({ aspect: gl.canvas.width / gl.canvas.height });
 		}
 
 		resize();
 
 		// Triangle that covers viewport, with UVs that still span 0 > 1 across viewport
-		const geometry = new Geometry(gl, {
-			position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
-			uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) }
-		});
+		// const geometry = new BufferGeometry({
+		// 	position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
+		// 	uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) }
+		// });
 		// Alternatively, you could use the Triangle class.
 
-		const program = new Program(gl, {
-			vertex,
-			fragment: /* glsl */ `
-                precision highp float;
-
-                varying vec3 vNormal;
-
-                void main() {
-                    vec3 normal = normalize(vNormal);
-                    float lighting = dot(normal, normalize(vec3(-0.3, 0.8, 0.6)));
-                    gl_FragColor.rgb = vec3(0., 0., 1.0) + lighting * 0.1;
-                    gl_FragColor.a = 1.0;
-                }
-            `,
-
-			cullFace: null,
-			uniforms: {
-				uTime: { value: 0 },
-				uResolution: { value: new Vec2(wW, wH) }
-			}
-		});
-
-		const planeGeom = new Plane(gl);
-
 		const video = document.querySelector('video');
-		const rect = video?.getBoundingClientRect();
-		console.log('🚀 ~ rect:', rect);
-		const plane = new Mesh(gl, { geometry: planeGeom, program });
-		plane.position.set(0, rect.top, 0);
-		plane.scale.set(rect.width, rect.height);
-		plane.setParent(scene);
 
-		const mesh = new Mesh(gl, { geometry, program });
+		const videoTex = new VideoTexture(video);
+		videoTex.encoding = sRGBEncoding;
+		// videoTex.minFilter = LinearFilter;
+		// videoTex.magFilter = LinearFilter;
+		// videoTex.format = RGBAIntegerFormat;
+
+		const material = new MeshBasicMaterial({ map: videoTex });
+
+		const rect = video?.getBoundingClientRect();
+
+		// const planeGeom = new PlaneGeometry(1, 1);
+		const planeGeom = new RoundedRect(rect.width, rect.height, 10, 64);
+		console.log('🚀 ~ rect:', rect);
+		const plane = new Mesh(planeGeom, material);
+
+		scene.add(plane);
+		plane.position.set(0, -rect.top, 0);
+		plane.scale.set(0.6 * 0.05, 0.6 * 0.05, 1.0);
 
 		window.addEventListener('resize', resize);
 
 		const raf = (time) => {
-			program.uniforms.uTime.value = time * 0.001;
-			// plane.position.set(0, $scroll.value * 0.1 || 0, 0);
+			// material.uniforms.uTime.value = time * 0.001;
+
 			const rect = video?.getBoundingClientRect();
 
-			console.log($glY, $glScale);
+			// console.log($glY, $glScale);
 			// const style = window.getComputedStyle(video);
 			// let matrix = new WebKitCSSMatrix(style.transform);
 
 			plane.position.set(
-				rect.x + rect.width * 0.5 - gl?.canvas.width * 0.5,
-				-rect.y - rect.height * 0.5 + gl?.canvas.height * 0.5 + $glY * wH,
+				rect.x + rect.width * 0.5 - renderer.getSize(vec).x * 0.5,
+				-rect.y - rect.height * 0.5 + renderer.getSize(vec).y * 0.5 + $glY * -wH,
 				1.0
 			);
 			console.log({ $glY, $glScale }, rect.width);
-			plane.scale.set(rect?.width * $glScale, rect?.height * $glScale, 1.0);
+			plane.scale.set($glScale, $glScale, 1.0);
 
-			camera.position.set(0, -$scroll.value || 0, z);
+			// camera.position.set(0, -$scroll.value || 0, z);
 			// plane.rotation.y += 0.02;
 
-			renderer.render({ scene, camera });
+			renderer.render(scene, camera);
 			requestAnimationFrame(raf);
 		};
 		requestAnimationFrame(raf);
